b224bb1e7f0ce19e9ba166832de0857d37875bad
max_sukh <maksym.sukharskyi@globacap.com>
2021-11-22 17:17:46 +0200

    Quadirlateral exersices unfinished (raw version)

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
-  # WRITE THIS CODE
+  if (a <= 0 || b <= 0 || c <= 0 || d <= 0)
+  raise QuadrilateralError
+  elsif ((a + b + c + d) != 360)
+    raise QuadrilateralError
+  elsif (a == 90 && b == 90 && c == 90 && d == 90)
+    quadrilateral = [:square,:rectangle]
+  elsif (a == b || a == c || a == d || b == c || b == d)
+    quadrilateral = [:parallelogram, :rhombus]
+  elsif (a != b || a != c || a != d || b != c || b != d)
+    quadrilateral = [:quadrilateral]
+  end
 end
 
 # Error class used in invalid_quadrilateral_spec. No need to change this code.
