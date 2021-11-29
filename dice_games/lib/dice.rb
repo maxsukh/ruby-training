106bd4d5e56a75f95d67ffac3c51adbb85ca0af8
max_sukh <maksym.sukharskyi@globacap.com>
2021-11-29 21:13:19 +0200

    Unfinished dice exercise (raw version)

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Dice
-  def roll(n)
-    @values = [n = 1]
+  def roll(n = 1)
+    @values = []
+    n.times { values << rand(1..6) }
   end
   def values()
     array = []
