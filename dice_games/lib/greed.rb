8695f437369906ea84c6c9659466213b20ebe1c2
max_sukh <maksym.sukharskyi@globacap.com>
2021-11-30 20:06:05 +0200

    A few more tests passed in greed

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class Greed
-  def score()
-    raise GreedError
+  greed = Greed.new
+  def score(greed)
+    if greed == []
+      return 0
+    elsif greed == [5]
+      return 50
+    elsif greed == [1]
+      return 100
+    elsif greed = [2] || [3] || [4] || [6]
+      return 0
+    else
+      raise GreedError
+    end
   end
 end
 
