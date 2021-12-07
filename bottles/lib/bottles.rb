773a20321ea04a1eacd21d5e5b7bfa7bc93ebf32
max_sukh <maksym.sukharskyi@globacap.com>
2021-12-07 21:01:39 +0200

    typical verse(1st test) refactoring(another way)

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class Bottles
-    def verse(song)
-        line_1 = "8 bottles of beer on the wall, 8 bottles of beer."
-        line_2 = "Take one down and pass it around, 7 bottles of beer on the wall."
-        "#{line_1}\n#{line_2}\n"
+    def verse(number)
+        number_of_bottles = 8
+        "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.\nTake one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall.\n"
     end
 end
