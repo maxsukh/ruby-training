b862962323534a966feaac1187ff2e326c2302fa
max_sukh <maksym.sukharskyi@globacap.com>
2021-12-09 14:41:31 +0200

    Condition added to pass the second test

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,8 +2,11 @@
 
 class Bottles
     @@bottles = 'bottles of beer'
-    def verse(number)
-        number_of_bottles = 8
-        "#{number_of_bottles} #{@@bottles} on the wall, #{number_of_bottles} #{@@bottles}.\nTake one down and pass it around, #{number_of_bottles - 1} #{@@bottles} on the wall.\n"
+    def verse(number_of_bottles)
+        @line_1_typical = "#{number_of_bottles} #{@@bottles} on the wall, #{number_of_bottles} #{@@bottles}."
+        @line_2_typical= "Take one down and pass it around, #{number_of_bottles - 1} #{@@bottles} on the wall."
+        if number_of_bottles == 8 || number_of_bottles == 3
+            "#{@line_1_typical}\n#{@line_2_typical}\n"
+        end
     end
 end
