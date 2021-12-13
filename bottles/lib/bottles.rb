3c9ea5e153dc9ced763452b216d0fd48131a630f
max_sukh <maksym.sukharskyi@globacap.com>
2021-12-13 09:58:45 +0200

    Added verses method

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,8 +9,10 @@ class Bottles
             "#{@line_1_typical}\n#{@line_2_typical}\n"
         elsif number_of_bottles == 1
             verse_1
-        elsif number_of_bottles = 2
+        elsif number_of_bottles == 2
             verse_2
+        elsif number_of_bottles == 0
+            verse_0
         end
     end
 
@@ -24,5 +26,15 @@ class Bottles
         @line_1 = "#{number_of_bottles} #{@@bottles} on the wall, #{number_of_bottles} #{@@bottles}."
         @line_2 = "Take one down and pass it around, 1 bottle of beer on the wall."
         "#{@line_1}\n#{@line_2}\n"
+    end
+
+    def verse_0(number_of_bottles = 0)
+        @line_1 = "No more #{@@bottles} on the wall, no more #{@@bottles}."
+        @line_2 = "Go to the store and buy some more, 99 #{@@bottles} on the wall."
+        "#{@line_1}\n#{@line_2}\n"
+    end
+
+    def verses(first, last)
+            first.downto(last).map {|item| verse(item)}.join("\n") + "\n"
     end
 end
