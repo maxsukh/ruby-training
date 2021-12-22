bb2af80cf12ccfc1ee0c7b6b52c89f4c51fa5ef6
max_sukh <maksym.sukharskyi@globacap.com>
2021-12-22 19:58:21 +0200

    Simple method created to display digit 8 properly.

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -1,5 +1,33 @@
 # frozen_string_literal: true
 
 class LCD
-  # Write your code in here
+  def result(digit)
+    if digit == 8
+      upper_part_of_digit(digit) + middle_part_of_digit(digit) + lower_part_of_digit(digit)
+    else expected = 
+        <<~SQUIGLY_HEREDOC
+              _
+             | |
+             |_|         
+       
+            SQUIGLY_HEREDOC
+        end
+    end
 end
+
+def upper_part_of_digit(number)
+    " _\n"      
+end
+
+def middle_part_of_digit(number)
+    "|_|\n"  
+end
+
+def lower_part_of_digit(number)
+    "|_| "
+end
+
+
+
+lcd = LCD.new
+puts lcd.result(8)
