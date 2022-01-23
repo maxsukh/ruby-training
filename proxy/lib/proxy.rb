3179916a18a52e5b74c2eee0162086fd3adbe3a3
max_sukh <maksym.sukharskyi@globacap.com>
2022-01-23 21:27:41 +0200

    Initial code wtitten. Few tests passed.

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -1,4 +1,28 @@
 # frozen_string_literal: true
 
 class Proxy
+  def initialize( proxy_object )
+    @proxy_object = proxy_object
+  end
+  
+  def method_missing(method_name, *args, &block)
+    if @proxy_object.respond_to?(method_name)
+      @proxy_object.send(method_name, *args, &block)
+    else
+      super(method_name, *args, &block)
+    end
+  end
+
+  def respond_to?(method_name, include_private = false)
+    if @proxy_object.respond_to?(method_name)
+      true
+    else
+      super(method_name, include_private)
+    end
+  end
+
+  def messages
+
+  end
+
 end
